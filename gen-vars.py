f667477918dda60eea45bad7377be46851002028
Bo-Chun Louis Chen <louistw@uab.edu>
2019-08-21 13:59:21 -0500

    Remove floating ip network id
    Since we use external network, no need to get the same id twice

--- gen-vars.py
+++ gen-vars.py
@@ -12,15 +12,12 @@ external_net_ID_val = subprocess.check_output('openstack network list --name {} 
 # get internal network id
 internal_net_ID_val = subprocess.check_output('openstack network list --name {} -c ID -f value'.format(internal_network), shell=True).strip()
 
-# get network id to attach floating ip
-instance_floating_ip_net_ID_val = subprocess.check_output('openstack network list --name {} -c ID -f value'.format(external_network), shell=True).strip()
-
 var = {} 
 
 var['source_image_name'] = 'CentOS-7-x86_64-GenericCloud-1905'
 var['external-net-ID'] = external_net_ID_val
 var['internal-net-ID'] = internal_net_ID_val
-var['instance_floating_ip_net-ID']= instance_floating_ip_net_ID_val
+var['instance_floating_ip_net-ID']= external_net_ID_val
 var['build_instance_name'] = 'ohpc'
 var['img_build_version'] ='0.3'
 var['ssh_username'] = 'centos'
