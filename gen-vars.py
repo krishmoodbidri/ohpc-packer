e22909352ddfe354e88cc0ff1ad70e7912596586
Bo-Chun Louis Chen <louistw@uab.edu>
2019-08-21 14:04:07 -0500

    Update variable names

--- gen-vars.py
+++ gen-vars.py
@@ -7,23 +7,23 @@ external_network = "dmznet"
 internal_network = "clusternet"
 
 # get external network id
-external_net_ID_val = check_output('openstack network list --name {} -c ID -f value'.format(external_network), shell=True).strip()
+external_net = check_output('openstack network list --name {} -c ID -f value'.format(external_network), shell=True).strip()
 
 # get internal network id
-internal_net_ID_val = check_output('openstack network list --name {} -c ID -f value'.format(internal_network), shell=True).strip()
+internal_net = check_output('openstack network list --name {} -c ID -f value'.format(internal_network), shell=True).strip()
 
 var = {} 
 
 var['source_image_name'] = 'CentOS-7-x86_64-GenericCloud-1905'
-var['external-net-ID'] = external_net_ID_val
-var['internal-net-ID'] = internal_net_ID_val
-var['instance_floating_ip_net-ID']= external_net_ID_val
+var['external-net'] = external_net
+var['internal-net'] = internal_net
+var['instance_floating_ip_net']= external_net
 var['build_instance_name'] = 'ohpc'
 var['img_build_version'] ='0.3'
 var['ssh_username'] = 'centos'
 var['ssh_keypair_name'] = 'Mac'
-var['private_key_file-path'] = '~/.ssh/id_rsa'
-var['ssh_host-IP'] = '164.111.161.138'
+var['private_key_file'] = '~/.ssh/id_rsa'
+var['ssh_host'] = '164.111.161.138'
 var['flavor'] = 'm1.medium'
 
 with open('vars-test.json', 'w') as f:  # writing JSON object 
