6e56a63db20667faaa1c1aa8f54b327344bd78e9
Bo-Chun Louis Chen <louistw@uab.edu>
2019-08-21 14:19:12 -0500

    Move var object definition to beginning
    Some fixed variable won't change after executing subprocess
     so move to beginning can easily change the value.

--- gen-vars.py
+++ gen-vars.py
@@ -6,25 +6,26 @@ import json
 external_network = "dmznet"
 internal_network = "clusternet"
 
+var = {
+    'build_instance_name': 'compute',
+    'build_version': '1',
+    'source_image_name': 'CentOS-7-x86_64-GenericCloud-1905',
+    'private_key_file': '~/.ssh/id_rsa',
+    'ssh_username': 'centos',
+    'ssh_keypair_name': 'os-gen-keypair',
+    'flavor': 'm1.medium'
+    }
+
 # get external network id
 external_net = check_output('openstack network list --name {} -c ID -f value'.format(external_network), shell=True).strip()
 
 # get internal network id
 internal_net = check_output('openstack network list --name {} -c ID -f value'.format(internal_network), shell=True).strip()
 
-var = {} 
 
-var['source_image_name'] = 'CentOS-7-x86_64-GenericCloud-1905'
 var['external-net'] = external_net
 var['internal-net'] = internal_net
 var['instance_floating_ip_net']= external_net
-var['build_instance_name'] = 'ohpc'
-var['img_build_version'] ='0.3'
-var['ssh_username'] = 'centos'
-var['ssh_keypair_name'] = 'Mac'
-var['private_key_file'] = '~/.ssh/id_rsa'
 var['ssh_host'] = '164.111.161.138'
-var['flavor'] = 'm1.medium'
-
 with open('vars-test.json', 'w') as f:  # writing JSON object 
     json.dump(var, f, indent=8)
